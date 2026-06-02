0e77420a3565d7021298d133a9a75b93670c07bb
theneth-ai <thenethdulshan@gmail.com>
2026-06-02 23:57:46 +0530

    Update my_name.py

--- my_name.py
+++ my_name.py
@@ -2,5 +2,5 @@ import sys
 try:
     print("Hello, my name is",sys.argv[1])
 except IndexError:
-    print("Please provide your name in the terminal!")
+    print("Please provide your first name in the terminal!")
     print("Ex: python [file name].py [Your name]")
