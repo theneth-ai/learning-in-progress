72d0625d23d1816e4ed9474e3fce5803d4babe7b
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-02 23:49:00 +0530

    Add command-line arguments example using sys.argv

---
+++ my_name.py
@@ -0,0 +1,6 @@
+import sys
+try:
+    print("Hello, my name is",sys.argv[1])
+except IndexError:
+    print("Please provide your name in the terminal!")
+    print("Ex: python [file name].py [Your name]")
