71e7ef3d98fc2a2080f1f288c6db036439d675ac
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-04 00:16:19 +0530

    Add error handling for command-line arguments in my_name.py

--- my_name.py
+++ my_name.py
@@ -1,6 +1,6 @@
 import sys
 try:
-    print("Hello, my name is",sys.argv[1])
+    print("Hello,my name is",sys.argv[1])
 except IndexError:
     print("Please provide your first name in the terminal!")
     print("Ex: python [file name].py [Your name]")
