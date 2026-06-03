0a9388dfab68cd644924fecdcbd4c2ed32503aee
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-04 00:19:32 +0530

    Refactor slices.py to demonstrate list slicing and error handling

--- slices.py
+++ slices.py
@@ -2,5 +2,5 @@ import sys
 if len(sys.argv)<2:
     sys.exit("Too few arguments") #Ends the program after displaying this message!
 
-for arg in sys.argv[1:]: #Slices the list from index 1 to the end of the list
+for arg in sys.argv[1:]: # Slices the list from index 1 to the end of the list
     print("Hello, my name is", arg) 
