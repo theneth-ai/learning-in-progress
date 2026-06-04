40b89d75a0d146adf6fedc72dccfa6bfb3f8ed63
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-04 22:45:00 +0530

    Add file_open.py and names_DB.txt database file

--- file_list.py
+++ file_list.py
@@ -2,7 +2,7 @@ name_list=[]
 count=0
 for x in range(3):
     name=input("Type your name: ")
-    name_list.append(name)
+    name_list.append(name)  #Add elements to the list 
 while count<len(name_list):
     print(f"This is {name_list[count]}")
     count=count+1
