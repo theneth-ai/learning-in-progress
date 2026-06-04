29c7091ae59cdb7d2993c23cf3f6f8da0b964109
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-05 00:20:44 +0530

    Process CSV data, format strings, and skip header using next()

--- nickname.py
+++ nickname.py
@@ -1,6 +1,6 @@
 people=[]
 with open("pet_Name.csv") as data:
-    next(data) #Igonres the first line in the csv file
+    next(data) #Ignores the first line in the csv file
     for line in data:
         name,nickname=line.rstrip().split(',')
         people.append(f"{name} called {nickname}")
