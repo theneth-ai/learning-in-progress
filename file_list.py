3de3c820db858aacec67bccf401aae9a4e2ac1ad
Theneth Dulshan <thenethdulshan@gmail.com>
2026-06-04 22:53:17 +0530

    Add list input logic before starting File I/O

--- file_list.py
+++ file_list.py
@@ -4,7 +4,7 @@ for x in range(3):
     name=input("Type your name: ")
     name_list.append(name)  #Add elements to the list 
 while count<len(name_list):
-    print(f"This is {name_list[count]}")
+    print(f"This is {name_list[count]}.")
     count=count+1
 
 print("Execution Completed!")
